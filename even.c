96264b6268afba427d939591ee3d9c1092e62342
abinithi <30343824+abinithi@users.noreply.github.com>
2017-08-04 16:55:19 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int number;
+	scanf("%d",&number);
+	if(number%2==0)
+	printf("%d is even.",number);
+	else
+	printf("%d is odd.",number);
+	return 0;
+}
+	
+
+
